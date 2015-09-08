7f46ea91004e61b17abc2e14e81ec0a4f14236cf
David Bernard <davidbernard8@gmail.com>
2015-09-07 21:20:29 -0400

    Commit 1
    Initial orientation of .js files and streamlining HTML

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Dave Bernard");
+var role = "Front-End Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+
+var Bio = {
+	"name" : "Dave Bernard",
+	"age" : "27",
+	"email" : "davidbernard8@gmail.com",
+	"photo" : images/me.jpg
+	"blurb" : "Hey, I'm Dave! Thanks for checking out my resume."
+};
+var formattedPic = HTMLbioPoc.replace("%data", images/me.jpg);
+$("#header").append(formattedPic);
